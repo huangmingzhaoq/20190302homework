f823cdebac1fb4f4edefa6473ba54a676449e84c
January14th <49264407+January14th@users.noreply.github.com>
2019-04-09 15:59:50 +0800

    阳漫
    0409作业

---
+++ 0409/Homework(1).java
@@ -0,0 +1,78 @@
+package com.one.text;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Homework {
+	public static void main(String[] args) {
+//第四章4月9日作业
+//一/
+//1，用户选择出拳：1.剪刀。2.石头。3.布
+//2，计算机随机出拳
+//3，判断胜负
+//4，询问是否继续
+//5，退出循环后，统计用户的胜率
+//		Scanner Scanner = new Scanner(System.in);
+//		String a ;
+//		int count=0,win=0;
+//		do {
+//			
+//		System.out.println("请选择出拳：1(剪刀),2(石头),3(布)");
+//		int me = Scanner.nextInt();
+//		System.out.println("计算机随机出拳:1(剪刀),2(石头),3(布)");
+//		Random random = new Random();
+//		int computer = random.nextInt(3);
+//		System.out.print("您输入的是"+me+"计算机输入的是"+computer);
+//		
+//		
+//		if (me == computer) {
+//			System.out.println("游戏结果为平局！");
+//			count+=1;
+//		}
+//		else if (me == 1 && computer == 3) {
+//			System.out.println("您胜利！");
+//			win+=1;
+//			count+=1;
+//		}
+//		else if (me == 2 && computer == 1) {
+//			System.out.println("您胜利！");
+//			win+=1;
+//			count+=1;
+//		}
+//		else if (me == 3 && computer == 2) {
+//			System.out.println("您胜利！");
+//			win+=1;
+//			count+=1;
+//		}
+//		else {
+//			System.out.println("计算机胜利！");
+//			count+=1;
+//		}
+//		
+//		System.out.println("是否继续猜拳游戏？yes（继续） /  no（停止）");
+//		a = Scanner.next();
+//		} while ("yes".equals(a));
+//			System.out.println("游戏结束！");
+//		   System.out.println("您的胜率为："+win/count*100+"%");
+
+	
+//二/
+//1.打印九九乘法表
+//1*1=1
+//1*2=2 2*2=4
+//1*3=3 2*3=6 3*3=9
+//1*4=4 2*4=8 3*4=12 4*4=16
+//1*5=5       
+//......
+//1*9=9 2*9=18...............9*9=81
+//		int a=0;
+//		for (int i = 1; i <= 9; i++) {
+//			for (int j = 1; j <=i; j++) {
+//				 a = i*j;
+//				 System.out.print(j+"*"+i+"="+a+"\t");
+//			}
+//			System.out.println();
+//		}
+		
+	}
+}
