bfe81efe98d6caaf7a81c1805db31dd5df06be03
陶济民 <49262069+taojimin@users.noreply.github.com>
2019-04-09 21:21:03 +0800

    刘文彪

---
+++ 0409/zuoye(4).java
@@ -0,0 +1,64 @@
+package lesson6;
+
+import java.util.Scanner;
+
+public class zuoye {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+//		int i=0,a=0,c=0;
+//		
+//		System.out.println("用户出拳(1为剪刀/2为石头/3为布);");
+//		int yonghuchuquan=scanner.nextInt();
+//		double suiji=Math.random()*3+1;
+//		suiji=(int)suiji;
+//		for( i=0;i<=9999;i++) {
+//			if(yonghuchuquan==1&&suiji==2||yonghuchuquan==2&&suiji==3||yonghuchuquan==3&&suiji==1) {
+//				System.out.println("计算器出的是"+suiji+"你出的是"+yonghuchuquan+"所以你输了");
+//			}
+//			i++;
+//		
+//			if(yonghuchuquan==2&&suiji==1||yonghuchuquan==3&&suiji==2||yonghuchuquan==1&&suiji==3) {
+//				System.out.println("计算器出的是"+suiji+"你出的是"+yonghuchuquan+"所以你赢了");
+//			}
+//			i++;
+//			a=i++;
+//			if(yonghuchuquan==suiji) {
+//				System.out.println("计算器出的是"+suiji+"你出的是"+yonghuchuquan+"所以平局");
+//			}
+//			i++;
+//			
+//		      System.out.println("是否继续(1/2)");
+//		      int jixu=scanner.nextInt();
+//		      if(jixu==1) {
+//		    	  System.out.println("用户出拳(1为剪刀/2为石头/3为布);");
+//		  		 yonghuchuquan=scanner.nextInt();
+//		      }
+//		      else {System.out.println("游戏结束");
+//		    	  break;}
+//		      }
+//		 if(c==a/i*10) {
+//			  System.out.println("用户胜率为");
+//		  }
+//		 System.out.println("用户胜率为"+c);
+//	       c=scanner.nextInt();
+		
+//		2.打印九九乘法表
+//		1*1=1
+//		1*2=2 2*2=4
+//		1*3=3 2*3=6 3*3=9
+//		1*4=4 2*4=8 3*4=12 4*4=16
+//		1*5=5       
+//		......
+//		1*9=9 2*9=18...............9*9=81
+//		for(int i=1;i<=9;i++) {
+//			for(int j=1;j<=i;j++) {
+//				System.out.print(i+"*"+j+"="+(i*j)+"\t");
+//			}
+//			System.out.println("");
+//			
+//		}
+		
+	}
+
+}
