dfeb2771e88ab7ab7f914d78ce2b5793a6911afe
陶济民 <49262069+taojimin@users.noreply.github.com>
2019-04-09 21:25:02 +0800

    刘维

---
+++ 0409/刘维.java
@@ -0,0 +1,77 @@
+package homework;
+import java.util.Scanner;
+import java.util.Random;
+public class Homeworkw {
+	
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);		
+//		Random  random=new  Random();
+//		1、用户选择出拳：1剪刀、2石头、3布
+//		      计算机随机出拳
+//		      判断胜负
+//		      询问是否继续
+//		      退出循环后，统计用户的胜率。
+//		String ans="y";
+//		double w=0,i=0;
+//		double win;
+//		while("y".equals(ans)); {
+//			int a,m=random.nextInt(3);
+//			System.out.println("用户选择:出拳1剪刀、2石头、3布");
+//			a=scanner.nextInt();
+//			if(a==1) {
+//				System.out.println("你出的是剪刀");
+//			}else if(a==2) {
+//				System.out.println("你出的是石头");
+//			}else if(a==3) {
+//				System.out.println("你出的是布");
+//			}
+//		   if(m==0 && a==2) {
+//			   System.out.println("对方出的是剪刀，你赢了");
+//			   w++;
+//			   i++;
+//		   }else if(m==0 && a==1) {
+//			   System.out.println("对方出的是剪刀，打平了");
+//			   i++;
+//		   }else if(m==0 && a==3) {
+//			   System.out.println("对方出的是剪刀，你输了");
+//			   i++;
+//		   }else if(m==1 && a==3) {
+//			   System.out.println("对方出的是剪刀，你赢了");
+//			   w++;
+//			   i++;
+//		   }else if(m==1 && a==2) {
+//			   System.out.println("对方出的是剪刀，打平了");
+//			   i++;
+//		   }else if(m==1 && a==1) {
+//			   System.out.println("对方出的是剪刀，你输了");
+//			   i++;
+//		   }else if(m==2 && a==1) {
+//			   System.out.println("对方出的是剪刀，你赢了");
+//			   w++;
+//			   i++;
+//		   }else if(m==2 && a==3) {
+//			   System.out.println("对方出的是剪刀，打平了");
+//			   i++;
+//		   }else if(m==2 && a==1) {
+//			   System.out.println("对方出的是剪刀，你输了");
+//			   i++;
+//		   }
+//		   System.out.println("是否继续？(y/n)");
+//		   Scanner scanner1=new Scanner(System.in);		
+//		   ans=scanner1.nextLine();
+//		   }
+//		   win=w/i;
+//		   System.out.println("胜率为："+win*100+"%");
+		
+//		 2.九九乘法表。
+//		for(int j=1;j<=9;j++) {
+//			for(int i=1;i<=j;i++) {
+//				System.out.print(i+"*"+j+"="+i*j+"\t");
+//			}
+//			System.out.println();
+//		}
+		
+		
+	}
+
+}
