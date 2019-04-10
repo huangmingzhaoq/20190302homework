3dd4f9d347ed29eab0dbfd495fa8af72c4c6c154
huangmingzhaoq <49300195+huangmingzhaoq@users.noreply.github.com>
2019-04-10 23:18:58 +0800

    Add files via upload

---
+++ 0410/黄茗沼.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+public class lianxi1 {
+	public static void main(String[] args) {
+	Scanner scanner=new Scanner(System.in);
+//	2.打印:
+//		1
+//		12 
+//		123
+//		1234
+//		12345
+//	System.out.println("打印");
+//	int sun=scanner.nextInt();
+//	for(int i=1;i<=sun;i++) {
+//		for(int j=1;j<=i;j++) {
+//	System.out.print(j);}
+//	System.out.println(" ");}
+	
+	
+	
+
+//3.打印:
+//54321
+//4321
+//321
+//21
+//1
+	
+//	System.out.println("打印");
+//	int sun;
+//	sun=scanner.nextInt();
+//	for(int i=1;i<=5;i++) {
+//		for(int j=6-i;j>=1;j--) {
+//			System.out.print(j+"");
+//		}
+//		System.out.println();
+//	}
+//	
+
+	  /*4.打印:
+		12345
+		1234
+		123
+		12
+		1*/
+	
+//	System.out.println("输入行数:");
+//	int num = scanner.nextInt();
+//	for(int i=1;i<=num;i++) {
+//		for(int j=1;j<=num-i+1;j++) {
+//			System.out.print(j);
+//		}
+//		System.out.println();
+//	}
+	
+	/*5.打印:
+	1
+	21
+	321
+	4321
+	54321*/
+
+
+	
+	
+	
+	
+	}
+
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
