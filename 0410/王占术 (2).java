2e93a1895311f106d43091f0e258da98f11a104e
刘红学 <49363149+Jin258@users.noreply.github.com>
2019-04-10 20:03:33 +0800

    王占术

---
+++ 0410/王占术 (2).java
@@ -0,0 +1,99 @@
+package work.space;
+
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		
+//		2.打印:
+//	    	1
+//	    	12 
+//	    	123
+//	    	1234
+//	    	12345
+	//   for(int i=1;i<=5;i++) {
+//		   for(int j=1;j<=i;j++) {
+//			   System.out.print(j);
+//		   }
+//		   System.out.println();
+	//   }
+	    
+	//   3.打印:
+//	    54321
+//	    4321
+//	    321
+//	    21
+//	    1
+	//   for(int i=5;i>=1;i--) {
+//		   for(int j=1;j<=i;j++) {
+//			   System.out.print(j);
+//		   }
+//		   System.out.println();
+	//   }
+
+	//4.打印:
+	//12345
+	//1234
+	//12
+	//12
+	//1
+//	    for(int i=5;i>0;i--) {
+//	    	for(int j=1;j<=i;j++) {
+//	    		System.out.print(j);
+//	    	}
+//	    	System.out.println();
+//	    }
+//		5.打印:
+//			1
+//			21
+//			321
+//			4321
+//			54321
+//		for(int i=1;i<=5;i++) {
+//			for(int j=i;j>=1;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		6.打印空心菱形。
+//	    *			1	1   1   4    
+//	   * *			2	3   2   3     
+//	  *   *			3	5   3   2      
+//	 *     *		4	7   4   1
+//	  *   * 		
+//	   * *
+//	    
+		
+		for(int i=1;i<=4;i++) {
+				for(int b=1;b<=5-i;b++) {
+					System.out.print(" ");}
+					for(int a=1;a<=i*2-1;a++) {	
+						 if(a==1||a==i*2-1) {
+							System.out.print("*");
+						}else  System.out.print(" ");
+					}
+				System.out.println();
+			}
+		for(int i=1;i<=3;i++) {
+			for(int b=1;b<=i+1;b++) {
+				System.out.print(" ");
+			}
+			for(int a=1;a<=(4-i)*2-1;a++) {
+				if(a==1||a==(4-i)*2-1) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		
+	}
+		
+	}
+
+
