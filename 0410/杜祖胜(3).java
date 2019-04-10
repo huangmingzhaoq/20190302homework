b223e1fcbddf376f075144ce337fc01776cf2e8e
ninghtWatchman <49261938+ninghtWatchman@users.noreply.github.com>
2019-04-10 19:42:58 +0800

    Create 杜祖胜(3).java

---
+++ 0410/杜祖胜(3).java
@@ -0,0 +1,95 @@
+package com.Test.day1;
+
+public class Project05 {
+
+	public static void main(String[] args) {
+		
+//		for(int a=1;a<=5;a++) {
+//			for(int b=1;b<=a;b++) {
+//				System.out.print(b);
+//			}System.out.println();
+//		}
+		
+		
+		
+//		for(int a=5;a>=1;a--) {
+//			for(int b=a;b>=1;b--) {
+//				System.out.print(b);
+//			}System.out.println();
+//		}
+		
+		
+
+		
+//		for(int a=5;a>=1;a--) {
+//			for(int b=1;b<=a;b++) {
+//				System.out.print(b);
+//			}System.out.println();
+//		}
+//		
+		
+		
+//		for(int a=1;a<=5;a++) {
+//			for(int b=a;b>=1;b--) {
+//				System.out.print(b);
+//			}System.out.println();
+//		}
+		
+		
+		
+//		Scanner input=new Scanner(System.in);
+//		int i,j,k;
+//		do{
+//			System.out.print("请输入一个奇数：");
+//			i = input.nextInt();
+//		}while(i%2==0);
+//		
+//		for (int h=1;h<=i;h++) {
+//			if (h<=i/2+1) {
+//				for(j=1;j<=i/2+1-h;j++) {
+//					System.out.print(" ");
+//				}
+//				for(k=1;k<=2*h-1;k++) {
+//					if(k==1||k==2*h-1) {
+//						System.out.print("*");
+//					}else {
+//						System.out.print(" ");
+//					}
+//				}
+//			}else {
+//				for(j=1;j<=h-(i+1)/2;j++) {
+//					System.out.print(" ");
+//				}
+//				for(k=1;k<=(i+1-h)*2-1;k++) {
+//					if(k==1||k==(i+1-h)*2-1) {
+//						System.out.print("*");
+//					}else {
+//						System.out.print(" ");
+//					}
+//				}
+//			}
+//			System.out.println();
+//		}
+		
+
+
+		
+		
+//		int a;
+//		int b;
+//		int c;
+//		for(a=0;a<=20;a++) {
+//			for(b=0;b<=33;b++) {
+//				c=100-a-b;
+//				if(c%3==0) {
+//					if(a*5+b*3+c/3==100) {
+//						System.out.println("母鸡买"+a+"只,"+"公鸡买"+b+"只,"+"小鸡买"+c+"只");
+//					}
+//				}
+//			}
+//		}
+		
+		
+	}
+
+}
