a83e16b3dbb6ed4b5cfe40b4d954209ad65b19d5
ninghtWatchman <49261938+ninghtWatchman@users.noreply.github.com>
2019-04-10 19:43:35 +0800

    Update ZY.java

--- 0409/ZY.java
+++ 0409/ZY.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //import java.util.Scanner;
 //public class ZY {
 //
@@ -39,3 +40,46 @@
 //	}
 //
 //}
+=======
+//import java.util.Scanner;
+//public class ZY {
+//
+//	public static void main(String[] args) {
+//		Scanner scanner=new Scanner(System.in);
+//		int a=1,c=0,d=0,e;
+//	while(a==1) {
+//		double b=Math.random()*10%3;
+//		b=(int)b;
+//		System.out.println("选择出1剪刀，2石头，0布");
+//		e=scanner.nextInt();
+//		if(e>2) {
+//			System.out.println("输入错误");
+//			continue;
+//		}
+//		c++;
+//		if(e==b) {
+//			System.out.println("平局");
+//		}
+//		if((b==0&&e==2)||(b==1&&e==0)||(b==2&&e==1)) {
+//			System.out.println("输了");
+//		}
+//		if((b==2&&e==0)||(b==0&&e==1)||(b==1&&e==2)) {
+//			System.out.println("赢了");
+//		}
+//		System.out.println("是否继续游戏1继续2不玩了");
+//		a=scanner.nextInt();
+//		}
+//	    System.out.println("游戏次数"+c);
+		
+//		for(int i=1;i<=9;i++) {
+//			for(int b=1;b<=i;b++) {
+//				System.out.println(b+"*"+i+"="+b*i+"\t");
+//			}
+//			System.out.println("");
+//		}
+//		
+//
+//	}
+//
+//}
+>>>>>>> b1af9a46f99bb1263b267cf8e74e37a7ac4503c4
