8e570f696285452d8f2067c8ea39f5a45edc13c5
January14th <49264407+January14th@users.noreply.github.com>
2019-04-10 14:11:58 +0800

    胡佳威
    0409作业

--- 0409/HomeWork.java
+++ 0409/HomeWork.java
@@ -1,113 +1,195 @@
-package com.newer.cn;
+package com.niuer.homework;
 
-import java.util.Random;
 import java.util.Scanner;
 
-public class Homework {
+public class HomeWork {
 
 	public static void main(String[] args) {
-//1.打印九九乘法表
-//1*1=1
-//1*2=2 2*2=4
-//1*3=3 2*3=6 3*3=9
-//1*4=4 2*4=8 3*4=12 4*4=16
-//1*5=5       
-//	......
-//1*9=9 2*9=18...............9*9=81
-//	    int i=1, j=1;
-//		for( i=1;i<10;i++) {
-//      for( j=1;j<=i;j++) {
-//         
-//         
-//        System.out.print(i+"*"+j+"="+i*j+"\t");
-//        	
-//       if(j==i) {
-//    	  System.out.print("\n");
-//       }
-//       }
-//      }
-//1、用户选择出拳：1剪刀、2石头、3布
-//2、计算机随机出拳
-//3、判断胜负
-//4、询问是否继续
-//5、退出循环后，统计用户的胜率。
-		int cp = 0, i;String ans = null;//接受字符串空号
-		int count = 0;
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("请输入你的数字：");
-		int user = scanner.nextInt();
-		Random random = new Random();
-		for (i = 1;; i++) {
-			if (user == 1) {
-				System.out.println("剪刀");
-			}
+		
+		//用户选择出拳  1剪刀 2石头 3布，计算机随机出拳，判断胜负，询问是否继续，退出程序，统计胜率
+		
+//		Scanner input=new Scanner(System.in);
+//		int cq;
+//		int jcq;
+//		String answer;
+//		int sum=0;
+//		int num=0;
+//		double i=0;
+//		do {
+//			System.out.print("请出拳（ 1剪刀 2石头 3布）：");
+//			cq=input.nextInt();
+//			if(cq!=1 && cq!=2 && cq!=3) {
+//				System.out.println("输入有误");
+//				break;
+//			}
+//			jcq=(int)(Math.random()*3+1);
+//			System.out.println("1--剪刀  2--石头  3--布\n您出的是"+cq+"计算机出的是"+jcq);	//可以加一句switch语句，或者写成方法
+//			
+//			if (cq==jcq) {
+//				System.out.println("平局");
+//			}else if(jcq==1&&cq==3 || jcq==2&&cq==1 || jcq==3&&cq==2) {
+//				System.out.println("计算机赢了");
+//			}else {
+//				System.out.println("你赢了");
+//				num++;
+//			}
+//			
+//			sum++;
+//			i=(num*1.0/sum*100);
+//			System.out.print("是否继续（y/n）:");
+//			answer=input.next();
+//			if(!"y".equals(answer)&&!"n".equals(answer)) {
+//				System.out.println("输入有误");
+//				break;
+//			}
+//			}while("y".equals(answer));
+//		System.out.println("一共进行了"+sum+"次，您一共赢了"+num+"次"+"\n您的胜率为"+i+"%");
 
-			if (user == 2) {
-				System.out.println("石头");
-			}
-
-			if (user == 3) {
-				System.out.println("布");
-			}
-
-			cp = (int) (random.nextInt(3) + 1);
-			if (cp == 1) {
-				System.out.println("计算机出的石头");
-			}
-			if (cp == 2) {
-				System.out.println("计算机出的剪刀");
-			}
-			if (cp == 3) {
-				System.out.println("计算机出的布");
-			}
-			if (user == 1 && cp == 1) {
-				System.out.println("电脑赢了");
-
-			}
-			if (user == 1 && cp == 2) {
-				System.out.println("平手");
-
-			}
-			if (user == 1 && cp == 3) {
-				System.out.println("你赢了");
-				count++;
-
-			}
-			if (user == 2 && cp == 1) {
-				System.out.println("平手");
-
-			}
-			if (user == 2 && cp == 2) {
-				System.out.println("你赢了");
-				count++;
-			}
-			if (user == 2 && cp == 3) {
-				System.out.println("电脑赢了");
-
-			}
-			if (user == 3 && cp == 1) {
-				System.out.println("你赢了");
-				count++;
-
-			}
-			if (user == 3 && cp == 2) {
-				System.out.println("电脑赢了");
-
-			}
-			if (user == 3 && cp == 3) {
-				System.out.println("平手");
-
-			}
-			System.out.println("是否还要继续" + "y/n");
-			ans = scanner.next();
-			if ("y".equals(ans)) {// 如果接受的是y继续循环否则停止循环
-			continue;
-
-					}
-			System.out.println(count++/i*100+"%");}
+		
+		
+		
+		
+		//1.打印九九乘法表
+		
+//		for (int i=1;i<=9;i++) {
+//			for (int j=1;j<=i;j++) {
+//				System.out.print(j+"*"+i+"="+i*j+"\t");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+		
+		
+		//--------------------------------------------------------------------------------
+		
+		//		2.打印:
+		//			1
+		//			12 
+		//			123
+		//			1234
+		//			12345
+		
+//		for (int i=1;i<=5;i++) {
+//			for (int j=1;j<=i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+		//		3.打印:
+		//			54321
+		//			4321
+		//			321
+		//			21
+		//			1
+		
+//		for (int i=5;i>=1;i--) {
+//			for (int j=i;j>=1;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+		//		4.打印:
+		//			12345
+		//			1234
+		//			123
+		//			12
+		//			1
+		
+//		for (int i=5;i>=1;i--) {
+//			for (int j=1;j<=i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+		//		5.打印:
+		//			1
+		//			21
+		//			321
+		//			4321
+		//			54321
+		
+//		for (int i=1;i<=5;i++) {
+//			for (int j=i;j>=1;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		
+		//		6.打印空心菱形。
+		//					行	       星	
+		//	    *			1      1
+		//	   * *			2      3
+		//	  *   *			3      5
+		//	 *     *		4	4  7	1
+		//	  *   * 		5	3  5	2
+		//	   * *			6	2  3	3
+		//	    *			7	1  1	4
+		
+//		Scanner input=new Scanner(System.in);
+//		int i,j,k;
+//		do{
+//			System.out.print("请输入一个奇数：");
+//			i = input.nextInt();
+//		}while(i%2==0);
+//		
+//		for (int h=1;h<=i;h++) {
+//			if (h<=i/2+1) {
+//				for(j=1;j<=i/2+1-h;j++) {
+//					System.out.print(" ");
+//				}
+//				for(k=1;k<=2*h-1;k++) {
+//					if(k==1||k==2*h-1) {
+//						System.out.print("*");
+//					}else {
+//						System.out.print(" ");
+//					}
+//				}
+//			}else {
+//				for(j=1;j<=h-(i+1)/2;j++) {
+//					System.out.print(" ");
+//				}
+//				for(k=1;k<=(i+1-h)*2-1;k++) {
+//					if(k==1||k==(i+1-h)*2-1) {
+//						System.out.print("*");
+//					}else {
+//						System.out.print(" ");
+//					}
+//				}
+//			}
+//			System.out.println();
+//		}
+	
+	
+		
+		//7.百元百鸡：母鸡5元一只，公鸡3元一只，小鸡1元3只，请输出100元买100只鸡的方案。
+		//母鸡	0-20	公鸡 0-33
+		
+//		int num5;
+//		int num3;
+//		int num1;
+//		for (num5=0;num5<=20;num5++) {
+//			for(num3=0;num3<=33;num3++) {
+//				num1=100-num5-num3;
+//				if(num1%3==0) {
+//					if(num5*5+num3*3+num1/3==100) {
+//						System.out.println("母鸡买"+num5+"只，公鸡买"+num3+"只，小鸡买"+num1+"只");
+//					}
+//				}
+//			}
+//		}
+		
 	}
-}
 
-//Random random=new Random();
-//int x=random.nextInt(3);//取0-2之间的随机数
-//System.out.println(x);
+}
