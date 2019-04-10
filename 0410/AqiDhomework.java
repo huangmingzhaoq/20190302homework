6ebc251219b7e0af41c877505266dbfdcf453e18
ninghtWatchman <49261938+ninghtWatchman@users.noreply.github.com>
2019-04-10 19:43:54 +0800

    Update AqiDhomework.java

--- 0410/AqiDhomework.java
+++ 0410/AqiDhomework.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.Aqi.demo;
 
 public class AqiDhomework {
@@ -77,3 +78,84 @@ public class AqiDhomework {
 	}
 
 }
+=======
+package com.Aqi.demo;
+
+public class AqiDhomework {
+
+	public static void main(String[] args) {
+//		1¡¢
+//		for(int j=1;j<=5;j++) {
+//		for(int i=1;i<=j;i++) {
+//			System.out.print(i);
+//		}
+//       System.out.println();
+//	}
+		//2¡¢
+//		for(int i=0;i<=5;i++) {	
+//			   for(int j=5-i;j>=1;j--) {
+//					System.out.print(j);
+//				}
+//	       System.out.println();
+//		}
+		//3¡¢
+//	       for(int j=0;j<=5;j++) {
+//	   		for(int i=1;i<=5-j;i++) {
+//	   			System.out.print(i);
+//	   		}
+//	         System.out.println();
+//	   	}
+		//4¡¢
+//	       for(int j=1;j<=5;j++) {
+//	   		for(int i=j;i>=1;i--) {
+//	   			System.out.print(i);
+//	   		}
+//	         System.out.println();
+//	   	}
+		//5¡¢
+//	       for(int i=1;i<=4;i++) {
+//				for(int j=1;j<=4-i;j++){
+//					System.out.print(" ");
+//				}			
+//					if(i==1) {
+//						System.out.println("*");
+//					}
+//					else if(i==2) {
+//						System.out.println("*"+" "+"*");
+//						}
+//					else if(i==3) {
+//						System.out.println("*"+" "+" "+" "+"*");
+//						}
+//					else if(i==4) {
+//						System.out.println("*"+" "+" "+" "+" "+" "+"*");
+//						}
+//					
+//				}
+//				for(int i=1;i<=3;i++) {
+//					for(int j=1;j<=i;j++) {
+//						System.out.print(" ");
+//					}
+//					if(i==1) {
+//						System.out.println("*"+" "+" "+" "+"*");
+//					}
+//					else if(i==2) {
+//						System.out.println("*"+" "+"*");
+//					}
+//					else if(i==3) {
+//						System.out.println("*");
+//					}
+//				}
+		//6¡¢
+//				for(int i=1;i<20;i++) {
+//			           for(int j=1;j<33;j++) {
+//			        	   int k=100-i-j;
+//			        	    if(3*j+5*i+k/3==100&&i+j+k==100&&k%3==0) {        	    		
+//			        			System.out.println("¹«¼¦:"+j+"Ä¸¼¦:"+i+"Ð¡¼¦:"+k);
+//			        			 }
+//			        	 }
+//			         }
+
+	}
+
+}
+>>>>>>> b1af9a46f99bb1263b267cf8e74e37a7ac4503c4
