a5d41ea3e1959bec588b47852585e1f7af7f74ff
ninghtWatchman <49261938+ninghtWatchman@users.noreply.github.com>
2019-04-10 19:43:01 +0800

    Create 刘维(1).java

---
+++ 0410/刘维(1).java
@@ -0,0 +1,154 @@
+package homework;
+
+public class Homeworke {
+
+	public static void main(String[] args) {
+//		1.九九乘法表
+//		for(int j=1;j<=9;j++) {
+//			for(int i=1;i<=j;i++) {
+//				System.out.print(i+"*"+j+"="+j*i+" ");
+//			}
+//			System.out.println();
+
+//		2.打印:
+//			1
+//			12 
+//			123
+//			1234
+//			12345
+		
+//		for(int i=1;i<=5;i++) {
+//			int num=1;
+//			for(int j=1;j<=i;j++) {
+//				System.out.print(num);
+//				if(num<i) {
+//					num++;
+//				}
+//			}
+//			System.out.println();
+//		}
+		
+//		    3.打印:
+//			54321
+//			4321
+//			321
+//			21
+//			1
+//		   for(int i=1;i<=5;i++) {
+//			   int num=6-i;
+//			   for(int j=1;j<=6-i;j++) {
+//				   System.out.print(num);
+//			   
+//			   if(num>0) { 
+//				   num--;
+//			   }
+//			   }
+//			   System.out.println();
+//		   }
+		 
+//		    4.打印:
+//			12345
+//			1234
+//			123
+//			12
+//			1
+//		   for(int i=1;i<=5;i++) {
+//			int num=1;
+//			for(int j=1;j<=6-i;j++) {
+//				System.out.print(num);
+//				if(num<6) {
+//					num++;
+//				}
+//			}
+//			System.out.println();
+//		}
+		
+//		5.打印:
+//			1
+//			21
+//			321
+//			4321
+//			54321
+//		for(int i=1;i<=5;i++) {
+//			int num=i;
+//		for(int j=1;j<=i;j++) {
+//			System.out.print(num);
+//			if(num>=1) {
+//				num--;
+//			}
+//		}
+//		System.out.println();
+//		}
+		
+//      6.打印空心菱形。
+//	         *
+//	        * *
+//	       *   *
+//	      *     *
+//	       *   * 
+//	        * *
+//	         *	
+//		   for(int i=1;i<=4;i++) {
+//			   for(int k=0;k<4-i;k++)
+//				   System.out.print(" ");
+//			   for(int j=1;j<=i*2-1;j++) {
+//				   if(j==1 || j==i*2-1) {
+//					   System.out.print("*");
+//			   }else {
+//				   System.out.print(" ");
+//			   }
+//			   }
+//			   System.out.println();
+//		   }
+//		   for(int i=1;i<=3;i++) {
+//			   for(int k=0;k<i;k++)
+//				   System.out.print(" ");
+//			   for(int j=1;j<=2*(3-i)+1;j++) {
+//				   if(j==1 || j==2*(3-i)+1) {
+//					   System.out.print("*");
+//				   }else {
+//					   System.out.print(" ");
+//				   }
+//			   }
+//			   System.out.println();
+//		   }
+		
+//		   7.百元百鸡：母鸡5元一只，公鸡3元一只，小鸡1元3只，请输出100元买100只鸡的方案。
+//		System.out.println("m表示母鸡，g表示公鸡，x表示小鸡！");
+//		int num=0;
+//		for(int m=0;m<=100;m++) {
+//			for(int g=0;g<=100;g++) {
+//				for(int x=0;x<=100;x=x+3) {
+//					if(m+g+x==100&&(5*m+3*g+x/3)==100) {
+//						num++;
+//						System.out.println("第"+num+"中种方案：");
+//						System.out.println("母鸡有"+m+"只，"+"公鸡有"+g+"只，"+"小鸡有"+x+"只。");
+//					}
+//				}
+//			}
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
