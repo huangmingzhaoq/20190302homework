47192d2349f2c1e7bd441a7a951878ec6923f7f2
ninghtWatchman <49261938+ninghtWatchman@users.noreply.github.com>
2019-04-10 19:43:04 +0800

    Create Zyliwenlong.java

---
+++ 0410/Zyliwenlong.java
@@ -0,0 +1,125 @@
+
+public class Zyliwenlong {
+	public static void main(String[] agrs) {
+
+//2.打印:
+//1
+//12 
+//123
+//1234
+//12345
+		//		for(int a=1;a<=5;a++) {
+//			for(int b=1;b<=a;b++) {
+//				System.out.print(b);
+//			}
+//			System.out.println();
+//		}
+//		3.打印:
+//			54321
+//			4321
+//			321
+//			21
+//			1
+//		int d=5;
+//		for(int a=1;a<=5;a++) {
+//			for(int b=5;a<=b;b--) {
+//				System.out.print(d);
+//				d--;
+//			}
+//			d=5;
+//			d=d-a;
+//			
+//			System.out.println();
+//		}
+//4.打印:
+//12345
+//1234
+//123
+//12
+//1
+//	int i=0;
+//		for(int a=1;a<=5;a++) {
+//			for(int b=5;a<=b;b--) {
+//				i++;
+//				System.out.print(i);
+//				
+//			}
+//			i=0;
+//			System.out.println();
+//		}
+//		5.打印:
+//			1
+//			21
+//			321
+//			4321
+//			54321
+//		int c=1;
+//		for(int a=1;a<=5;a++) {
+//			for(int b=1;b<=a;b++) {
+//				System.out.print(c);
+//				c--;
+//			}
+//			System.out.println();
+//			c=1;
+//			c=c+a;
+//		}
+//
+//6.打印空心菱形。
+//    *
+//   * *
+//  *   *
+// *     *
+//  *   * 
+//   * *
+//    *
+//		for(int a=1;a<=4;a++) {
+//			for(int b=3;a<=b;b--) {
+//				System.out.print(" ");
+//			}
+//			for(int c=1;c<=2&&c<=a;c++) {
+//				System.out.print("*");
+//				if(a==2) {
+//					for(int e=1;e<=1;e++) {
+//					System.out.print(" ");}
+//				}
+//				if(a==3) {
+//					for(int e=1;e<=3;e++) {
+//					System.out.print(" ");}
+//				}
+//				if(a==4) {
+//					for(int e=1;e<=5;e++) {
+//					System.out.print(" ");}
+//				}
+//			}
+//			System.out.println();
+//		}
+//		for(int i=1;i<=3;i++) {
+//			for(int f=1;f<=i;f++) {
+//				System.out.print(" ");
+//			}
+//			for(int g=1;g<=2;g++) {
+//				if(g==2&&i==1) {
+//				System.out.print("*"+" "+" "+" "+"*");
+//				}
+//				if(i==2&&g==2) {
+//					System.out.print("*"+" "+"*");
+//				}
+//			}
+//			for(int a=i;a==3;a++) {
+//				System.out.println("*");
+//			}
+//			System.out.println();
+//			}
+		//7.百元百鸡：母鸡5元一只，公鸡3元一只，小鸡1元3只，请输出100元买100只鸡的方案。
+//		int a,b;
+//		for(a=1;a<20;a++) {
+//			for(b=1;b<33;b++) {
+//				int k=100-b-a;
+//				if(a*5+b*3+k/3==100&&k%3==0&&a+b+k==100) {
+//					System.out.println("可以买母鸡"+a+"只"+",公鸡"+b+"只"+"小鸡"+k+"只");
+//				}
+//			}
+//		}
+//		
+}
+}
