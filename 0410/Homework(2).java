9ccd232ba2ee835fe95a77d867634144edf8c90a
陶济民 <49262069+taojimin@users.noreply.github.com>
2019-04-10 21:27:52 +0800

    吴俊

---
+++ 0410/Homework(2).java
@@ -0,0 +1,125 @@
+package newer.com.demo;
+
+import java.util.Scanner;
+
+/**
+ * 打印:
+1
+12 
+123
+1234
+12345
+ * @author Administrator
+ *
+ */
+public class Homework {
+
+	public static void main(String[] args) {
+//		for(int i=1;i<=5;i++) {
+//			for(int j=1;j<=i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		/**
+		 * 打印:
+		54321
+		4321
+		321
+		21
+		1
+		 * @author Administrator
+		 *
+		 */
+//		for(int i=1;i<=5;i++) {
+//			for( int j=6-i;j>=1;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		/**
+		 * 打印
+		 * 12345
+			1234
+			123
+			12
+			1
+		 * @author Administrator
+		 *
+		 */
+//		for(int i=1;i<=5;i++) {
+//			for(int j=1;j<=6-i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		/**
+		 * 打印:
+		1
+		21
+		321
+		4321
+		54321
+		 * @author Administrator
+		 *
+		 */
+//		for(int i=1;i<=5;i++) {
+//			for(int j=i;j>=1;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+//		打印空心棱形
+		
+		
+		
+//		Scanner input=new Scanner(System.in);
+//		System.out.println("请输入行数");
+//	    int num=input.nextInt();
+//	    
+//	    for(int i=1;i<=(num/2+1);i++) {
+//	    	for(int j=1;j<=num/2+1-i;j++){
+//	    		System.out.print(" ");
+//	    	}
+//	    	for(int j=1;j<=2*i-1;j++) {
+//	    		if((j==1)||j==(2*i-1)) {
+//	    			System.out.print("*");
+//	    		}else {
+//	    			System.out.print(" ");
+//	    		}
+//	    	}
+//	    System.out.println();
+//	    }
+//	    for(int i=1;i<=(num/2);i++) {
+//	    	for(int j=1;j<=i;j++) {
+//	    		System.out.print(" ");
+//	    	}
+//	    	for(int j=1;j<=(num-2*i);j++) {
+//	    		if((j==1)||j==(num-2*i)) {
+//	    			System.out.print("*");
+//	    		}else {
+//	    			System.out.print(" ");
+//	    		}
+//	    		
+//	    		
+//	    	}
+//	    	System.out.println();
+//	    }
+//		
+//		百元百鸡
+//		for(int i=1;i<=20;i++) {
+//			for(int j=1;j<=33;j++) {
+//				int k=100-i-j;
+//				if((i+j+k)==100&&(i*5+3*j+k/3.0)==100) {
+//					System.out.println("母鸡买"+i+"只"+"公鸡买"+j+"只"+"小鸡买"+k+"只");
+//				}
+//			}
+//		}
+		
+		
+
+
+	}
+
+}
